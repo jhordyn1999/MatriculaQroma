a865c2b61ff83a245cd3e882962ad745a7b47f6b
Jhordyn1999 <jhordynalexander10@gmail.com>
2022-05-17 11:12:05 -0500

    version2

--- index.php
+++ index.php
@@ -1,30 +1,51 @@
 <?php
-require_once(__DIR__ . '/../config.php');
+// require_once(__DIR__ . '/../config.php');
 
-global $PAGE, $DB, $USER, $SITE;
+// global $PAGE, $DB, $USER, $SITE;
+
+// require_login();
+
+// $urlparams = array();
+// $PAGE->set_url('/matricula/', $urlparams);
+
+// $PAGE->set_pagetype('site-matricula');
+// $PAGE->set_title($SITE->fullname);
+
+// $rolesArr = [];
+// $userRol = $DB->get_records_sql("SELECT * FROM {role_assignments} WHERE userid=?",array($USER->id));
+
+// foreach($userRol as $ur) {
+// 	$rolesArr[] = $ur->roleid;
+// }
+
+// if(!in_array(1, $rolesArr) && !is_siteadmin()) {
+// 	header("Location: https://aulavirtual.urbanova.com.pe");
+// 	exit();
+// }
+
+// echo $OUTPUT->header();
+
+// include('home.html');
+
+// echo $OUTPUT->footer();
+
+require_once(__DIR__ . '/../../config.php');
+global $DB;
 
 require_login();
 
-$urlparams = array();
-$PAGE->set_url('/matricula/', $urlparams);
+$context = context_system::instance();
+$PAGE->set_url(new moodle_url('/local/mundo_qroma/index.php'));
+$PAGE->set_context(\context_system::instance());
+$PAGE->set_title('Mundo Qroma');
+$PAGE->set_heading('Mundo Qroma');
 
-$PAGE->set_pagetype('site-matricula');
-$PAGE->set_title($SITE->fullname);
 
-$rolesArr = [];
-$userRol = $DB->get_records_sql("SELECT * FROM {role_assignments} WHERE userid=?",array($USER->id));
-
-foreach($userRol as $ur) {
-	$rolesArr[] = $ur->roleid;
-}
-
-if(!in_array(1, $rolesArr) && !is_siteadmin()) {
-	header("Location: https://aulavirtual.urbanova.com.pe");
-	exit();
-}
+$templateContext = (object)[
+    'sesskey' => sesskey()
+];
 
 echo $OUTPUT->header();
-
-include('home.html');
-
+// echo $OUTPUT->render_from_template('local_mundo_qroma/index', $templateContext);
+include('index.html');
 echo $OUTPUT->footer();
